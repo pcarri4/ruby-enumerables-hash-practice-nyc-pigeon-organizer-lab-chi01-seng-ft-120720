a14c849e4eff1ba4a7ebecccc462ba8b840b134a
Paulina Carrillo <paulinacarri4@gmail.com>
2020-11-29 05:24:29 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,21 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  
+  
+  new_hash = {}
+  
+  data.each do |key_category, value|
+    value.each do |key_specific_items, array|
+      array.each do |name|
+        if new_hash[name] == nil 
+          new_hash[name] ={}
+        end
+        if new_hash[name][key_category] == nil
+          new_hash[name][key_category] = []
+        end
+        new_hash[name][key_category].push(key_specific_items.to_s)
+      end
+    end
+  end
+  new_hash
 end
